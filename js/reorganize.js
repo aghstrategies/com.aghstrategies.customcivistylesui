cdde599492512bafcbe1cd1d71ff3822404a62ba
AGH Strategies <info@aghstrategies.com>
2019-03-18 16:58:52 -0400

    other amount

--- js/reorganize.js
+++ js/reorganize.js
@@ -2,4 +2,9 @@ CRM.$(function ($) {
   $('div.crm-contribution-main-form-block').wrapInner('<div class="half">');
   $('div#intro_text').insertBefore('.half').addClass('half');
   $('div.messages').insertBefore('div#intro_text');
+ 
+  // other amount
+  var other = $('.price-set-row input[value="0"]').attr('id');
+  console.log(other);
+  $('.other_amount-section').insertAfter('.price-set-row label[for="' + other + '"]');
 });
