31037959a10b3db77f6c9ce83560563a5d82e2f0
Alice Frumin <alice@aghstrategies.com>
2021-02-25 13:23:12 -0500

    cleanup

--- js/reorganize.js
+++ js/reorganize.js
@@ -1,7 +1,9 @@
 CRM.$(function ($) {
- 
+
   // other amount
   var other = $('.price-set-row input[value="0"]').attr('id');
   console.log(other);
-  $('.other_amount-section').insertAfter('.price-set-row label[for="' + other + '"]');
+  // $('.other_amount-section').insertAfter('.price-set-row label[for="' + other + '"]');
+  $('label[for="' + other + '"]').hide();
+  $('.other_amount-content input').attr('placeholder', 'Other');
 });
