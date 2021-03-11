f74f20a358ba0465d32184a4886a28f5856c9ac9
AGH Strategies LLC <info@aghstrategies.com>
2021-03-11 14:22:10 -0500

    styled buttons

--- js/reorganize.js
+++ js/reorganize.js
@@ -1,9 +1,13 @@
 CRM.$(function ($) {
+  $('div#intro_text').insertBefore('div.crm-contribution-main-form-block');
+ 
+   var other = $('.price-set-row input[value="0"]').attr('id');
+   console.log(other);
+   // $('.other_amount-section').insertAfter('.price-set-row label[for="' + other + '"]');
+   $('label[for="' + other + '"]').hide();
 
+  $('.other_amount-section').insertAfter('.price-set-row:last');
+  
   // other amount
-  var other = $('.price-set-row input[value="0"]').attr('id');
-  console.log(other);
-  // $('.other_amount-section').insertAfter('.price-set-row label[for="' + other + '"]');
-  $('label[for="' + other + '"]').hide();
-  $('.other_amount-content input').attr('placeholder', 'Other');
+  $('.other_amount-content input').attr('placeholder', '$0.00');
 });
