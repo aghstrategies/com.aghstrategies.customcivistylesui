bb2c0efce4fc8fbeb01016da7f1d139958b09249
Alice Frumin <alice@aghstrategies.com>
2026-02-17 16:34:46 -0500

    update civix

--- customcivistylesui.php
+++ customcivistylesui.php
@@ -53,46 +53,12 @@ function customcivistylesui_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function customcivistylesui_civicrm_uninstall() {
-  _customcivistylesui_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function customcivistylesui_civicrm_enable() {
   _customcivistylesui_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function customcivistylesui_civicrm_disable() {
-  _customcivistylesui_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function customcivistylesui_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _customcivistylesui_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
